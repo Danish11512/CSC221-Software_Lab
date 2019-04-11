426cafef853222c7572955522a90ffe67e100630
Danish Faruqi <danish.faruqi1@gmail.com>
2019-04-11 10:52:25 -0400

    A Java Fx Mortgage Calculator

---
+++ MortgageCalculator/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MortgageCalculator.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Mortgage Calculator");
+        stage.setScene(scene);
+        stage.show();
+//        primaryStage.setTitle("Mortgage Calculator");
+//        primaryStage.setScene(new Scene(root, 500, 603));
+//        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
